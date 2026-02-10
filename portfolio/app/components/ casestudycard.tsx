9b6b024d19860dd482875d515827e27db81c04eb
devmjkhan@gmail.com <devmjkhan@gmail.com>
2026-02-10 20:58:20 +0500

    changes again

--- portfolio/app/components/ casestudycard.tsx
+++ portfolio/app/components/ casestudycard.tsx
@@ -7,7 +7,7 @@ type CaseStudyCardProps = {
 const CaseStudyCard = ({ title, client, image }: CaseStudyCardProps) => {
   return (
     <div className="flex flex-col gap-1 ">
-      <div className="w-full  sm:h-96 h-60.5 bg-[#f4f4f4] border border-[#eeeeee] overflow-hidden">
+      <div className="w-full sm:h-96 h-60.5 bg-[#f4f4f4] border border-[#eeeeee] overflow-hidden">
         {image && <img src={image} alt={title} className="w-full h-full object-cover" />}
       </div>
       <div className="flex flex-col gap-1 mt-2">
