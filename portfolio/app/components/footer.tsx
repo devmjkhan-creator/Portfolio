195dc367b37e0d14e2e19f3c3a56376f9a51c9d3
devmjkhan@gmail.com <devmjkhan@gmail.com>
2026-02-11 11:16:06 +0500

    chnges

--- portfolio/app/components/footer.tsx
+++ portfolio/app/components/footer.tsx
@@ -19,53 +19,61 @@ const Footer = () => {
         {/* ............................ card & links............................... */}
 
         {/* container */}
-        <div className='flex flex-row sm:gap-[0.8vw] gap-3 items-center justify-between '>
+        <div className='flex flex-row sm:gap-[0.8vw] gap-3 '>
          {/* innerblock1... */}
-           <div className='gap-[14.73px] flex flex-col items-center '>
+
+
+            {/* button1 */}
+           <div className='gap-[14.73px] flex flex-col items-center  '>
             <div
-            className='Card relative rounded-lg bg-center object-cover  image-rendering-auto'>
-            <img src="/pictures/twittercard.svg" alt="TwitterCard" className='w-full h-full bg-center object-cover ' />
-             <div> <img
-            src="/pictures/twitter.svg"
-            alt="TwitterLogo"
-            className="absolute -left-6 bottom-1 w-11 h-11 object-contain"
-            style={{
-    imageRendering: 'auto',
-    shapeRendering: 'geometricPrecision'
-            }}
-/>
-                    </div>
+            className='Card relative rounded-lg bg-center object-cover w-22.5 h-22.5 image-rendering-auto overflow-visible'>
+            <img src="/pictures/twittercard.svg" alt="TwitterCard" className='w-full h-full bg-center object-cover overflow-visible ' />
+             <div> <img src="/pictures/twitter.svg" alt="TwitterLogo" className="absolute -left-6 bottom-1 w-11 h-11 object-contain"/> </div>
             </div>
            <button
-           className=' text-[14px] sm:w-[7vw] w-20 sm:h-[4.40vh] h-9 bg-[#F4F4F4]  tracking-[-0.14%] leading-5 sm:px-[22pxvw] px-5.5 sm:py-2 py-2 border-l border-r rounded-4xl border-[#f4f4f4] border-t border-b items-center flex justify-center'>Twitter
+           className='mt-auto text-[14px] sm:w-[7vw] w-20 sm:h-[4.40vh] h-9 bg-[#F4F4F4]  tracking-[-0.14%] leading-5 sm:px-[22pxvw] px-5.5 sm:py-2 py-2 border-l border-r rounded-4xl border-[#f4f4f4] border-t border-b items-center flex justify-center'>Twitter
            </button>
            </div>
 
+
+            {/* button2 */}
            <div className='gap-[14.73px] flex flex-col items-center'>
             <div
-             className='Card relative rounded-lg bg-center object-cover  image-rendering-auto'>
-               <img src="/pictures/dribblecard.svg" alt="DribbleCard" className='w-full h-full bg-center object-cover ' />
-             <div> <img src="/pictures/dribble.svg" alt="Dribblelogo" className='absolute bg-center object-contain  bottom-1 -left-6 ' /></div>
+             className='Card relative rounded-lg bg-center object-cover w-22.5 h-22.5  image-rendering-auto  overflow-visible '>
+               <img src="/pictures/dribblecard.svg" alt="DribbleCard" className='w-full h-full bg-center object-cover  overflow-visible  ' />
+             <div> <img src="/pictures/dribble.svg" alt="Dribblelogo" className='w-11 h-11  overflow-visible  absolute bg-center object-contain bottom-1 -left-6 ' /></div>
              </div>
        
            <button 
-           className='text-[14px]  sm:w-[7vw] w-20 sm:h-[4.40vh] h-9 bg-[#f4f4f4]  tracking-[-0.14%] leading-5 sm:px-[22pxvw] px-5.5 sm:py-2 py-2 border-l border-r rounded-4xl border-[#f4f4f4] border-t border-b items-center flex justify-center'>Dribbble
+           className=' mt-auto text-[14px]  sm:w-[7vw] w-20 sm:h-[4.40vh] h-9 bg-[#f4f4f4]  tracking-[-0.14%] leading-5 sm:px-[22pxvw] px-5.5 sm:py-2 py-2 border-l border-r rounded-4xl border-[#f4f4f4] border-t border-b items-center flex justify-center'>Dribbble
            </button>
            </div>
 
+
+           {/* button3 */}
            <div className='gap-[14.73px] flex flex-col items-center '>
             <div
-             className='Card  relative  rounded-lg bg-center object-cover  image-rendering-auto'>
-               <img src="/pictures/linkidencard.svg" alt="LinkidenCard" className='w-full h-full bg-center object-cover ' />
+             className='Card  relative  rounded-lg bg-center object-cover  image-rendering-auto w-22.5 h-22.5  overflow-visible '>
+               <img src="/pictures/linkidencard.svg" alt="LinkidenCard" className='w-full h-full bg-center object-cover  overflow-visible  ' />
                <div><img src="/pictures/linkiden.svg" alt="Linkidenlogo" className='absolute bg-center object-contain bottom-3 -left-6 ' /></div>
              </div>
-           <button className='text-[14px]   sm:w-[7vw] w-20 sm:h-[4.40vh] h-9 bg-[#f4f4f4]  tracking-[-0.14%] leading-5 sm:px-[22pxvw] px-5.5 sm:py-2 py-2 border-l border-r rounded-4xl border-[#f4f4f4] border-t border-b items-center flex justify-center'>LinkedIn</button>
+           <button
+            className='text-[14px]   sm:w-[7vw] w-20 sm:h-[4.40vh] h-9 bg-[#f4f4f4]  tracking-[-0.14%] leading-5 sm:px-[22pxvw] px-5.5 sm:py-2 py-2 border-l border-r rounded-4xl border-[#f4f4f4] border-t border-b items-center flex justify-center'>LinkedIn
+            </button>
            </div>
+
+
 
         </div>
        
       </div>
     </div>
+
+
+
+
+
+
     {/* block6 */}
     <div className='w-full  flex items-center justify-center font-inter'>
       <div className='w-full sm:w-[39.875em] h-full sm:border-r border-r-(--color-border)  sm:border-l border-l-(--color-border) sm:py-10 py-8 items-center flex justify-center'>
